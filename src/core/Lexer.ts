74b7c20ca22b7e5fe5c22dba439fb753c2924c9a
JaimermXD <jairegra@gmail.com>
2022-05-04 11:06:20 +0200

    Implement baseline Lexer class

---
+++ src/core/Lexer.ts
@@ -0,0 +1,16 @@
+import { TokenType, Token } from "../components/index";
+import { UnexpectedClassError } from "../errors/UnexpectedCharError";
+
+export class Lexer {
+    input: string;
+    start: number;
+    current: number;
+    tokens: Token[];
+
+    constructor(input: string) {
+        this.input = input;
+        this.start = 0;
+        this.current = 0;
+        this.tokens = [];
+    }
+}
