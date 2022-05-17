a06c48426c188e25e7ca6694b972b0faf2a2c1b9
Jaime Redondo <jairegra@gmail.com>
2022-05-17 16:25:06 +0200

    Implement interpreter functionality

--- src/core/Interpreter.ts
+++ src/core/Interpreter.ts
@@ -12,4 +12,65 @@ export class Interpreter implements Expr.Visitor<boolean> {
         this.values = {};
         this.results = [];
     }
+
+    generateTruthTable(variables: Token[]): Table {
+        this.results = [];
+
+        const table: Table = [];
+
+        for (let i = 0; i < Math.pow(2, variables.length); i++) {
+            let binary = i.toString(2);
+            binary = "0".repeat(variables.length - binary.length) + binary;
+            
+            const values: TableRow = {};
+            for (let j = 0; j < variables.length; j++) {
+                values[variables[j].value] = !!+binary.charAt(j);
+            }
+
+            const formula = new AstFormatter(this.tree).format();
+            const result = this.interpret(values);
+            values[formula] = result;
+            this.results.push(result);
+            table.push(values);
+        }
+
+        return table;
+    }
+
+    private interpret(values: TableRow): boolean {
+        this.values = values;
+        return this.tree.accept(this);
+    }
+
+    visitUnaryExpr(expression: Expr.Unary): boolean {
+        const right = this.evaluate(expression.right);
+        return !right;
+    }
+
+    visitBinaryExpr(expression: Expr.Binary): boolean {
+        const left = this.evaluate(expression.left);
+        const right = this.evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case TokenType.CONJUNCTION: return left && right;
+            case TokenType.DISJUNCTION: return left || right;
+            case TokenType.IMPLICATION: return !left || right
+            case TokenType.EQUIVALENCE: return left === right;
+        }
+
+        // Unreachable
+        return false;
+    }
+
+    visitVariableExpr(expression: Expr.Variable): boolean {
+        return this.values[expression.name];
+    }
+
+    visitGroupingExpr(expression: Expr.Grouping): boolean {
+        return this.evaluate(expression.expression);
+    }
+
+    private evaluate(expression: Expr): boolean {
+        return expression.accept(this);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { question as prompt } from "readline-sync";
 
-import { Lexer, Parser } from "./core/index";
+import { Lexer, Parser, Interpreter } from "./core/index";
+import { printTable } from "./utils/printTable";
 
 while (true) {
     try {
@@ -13,6 +14,13 @@ while (true) {
 
         const parser = new Parser(tokens);
         const tree = parser.parse();
+
+        const interpreter = new Interpreter(tree);
+        const table = interpreter.generateTruthTable(lexer.getVariables());
+
+        console.log();
+        printTable(table);
+        console.log(`Formula is ${interpreter.results.every(Boolean) ? "a tautology" : interpreter.results.some(Boolean) ? "satisfiable" : "a contradiction"}\n`);
     } catch (err) {
         if (err instanceof Error) console.error(`Error: ${err.message}`);
     }
