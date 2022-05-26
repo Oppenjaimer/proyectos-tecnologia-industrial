883d297362b404b759bcac15b19cce9bc5f2919f
JaimermXD <jairegra@gmail.com>
2022-05-26 17:47:19 +0200

    Format Python files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from simulation import Simulation
 
+
 def main():
     doors = 3
     attempts = 1000000
@@ -11,15 +12,16 @@ def main():
     for _ in range(attempts):
         if simulation.simulate(False):
             wins_no_switch += 1
-        
+
         if simulation.simulate(True):
             wins_switch += 1
-    
+
     percentage_switch = wins_switch / attempts * 100
     percentage_no_switch = wins_no_switch / attempts * 100
 
     print(f"    Switching: {wins_switch:,} wins / {attempts:,} attempts ({percentage_switch:.2f}% winning percentage)")
     print(f"Not switching: {wins_no_switch:,} wins / {attempts:,} attempts ({percentage_no_switch:.2f}% winning percentage)")
+
 
 if __name__ == "__main__":
     main()
--- simulation.py
+++ simulation.py
@@ -1,16 +1,17 @@
 from random import randrange
 
+
 class Simulation:
     def __init__(self, doors: int):
         self.doors = doors
-    
+
     def pick_door(self) -> int:
         return randrange(0, self.doors)
-    
+
     def simulate(self, switch: bool) -> bool:
         prize_door = self.pick_door()
         chosen_door = self.pick_door()
-        
+
         closed_doors = list(range(0, self.doors))
         while len(closed_doors) > 2:
             door_to_open = self.pick_door()
@@ -20,7 +21,7 @@ class Simulation:
 
             if door_to_open not in [prize_door, chosen_door]:
                 closed_doors.remove(door_to_open)
-        
+
         if switch:
             chosen_door = sum(closed_doors) - chosen_door
 
