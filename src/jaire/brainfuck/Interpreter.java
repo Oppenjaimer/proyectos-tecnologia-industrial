6b38d03c1975d6e729d18f172bb2dafaa990b64f
JaimermXD <jairegra@gmail.com>
2022-05-26 17:32:49 +0200

    Implement baseline Interpreter class

---
+++ src/jaire/brainfuck/Interpreter.java
@@ -0,0 +1,49 @@
+package jaire.brainfuck;
+
+import java.util.Scanner;
+
+public class Interpreter {
+    private final int LENGTH = 65535;
+    private final int[] mem = new int[LENGTH];
+    private int ptr = 0;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void interpret(String code) {
+        code = code.replaceAll("[^><+\\-.,\\[\\]]", "");
+
+        if (!isBalanced(code)) {
+            System.err.println("Error: unmatched brackets");
+            return;
+        }
+
+        int j = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char cmd = code.charAt(i);
+
+            switch (cmd) {
+                case '>' -> ptr = (ptr == LENGTH - 1) ? 0 : ptr + 1;
+                case '<' -> ptr = (ptr == 0) ? LENGTH - 1 : ptr - 1;
+                case '+' -> mem[ptr] = unsigned(mem[ptr] + 1);
+                case '-' -> mem[ptr] = unsigned(mem[ptr] - 1);
+                case '.' -> System.out.print((char) unsigned(mem[ptr]));
+                case ',' -> mem[ptr] = unsigned(scanner.next().charAt(0));
+                // TODO: implement code for brackets
+            }
+        }
+    }
+
+    private boolean isBalanced(String str) {
+        int paired = 0;
+
+        for (int i = 0; i < str.length() && paired >= 0; i++) {
+            if (str.charAt(i) == '[') paired++;
+            else if (str.charAt(i) == ']') paired--;
+        }
+
+        return paired == 0;
+    }
+
+    private int unsigned(int signed) {
+        return signed & 0xff;
+    }
+}
