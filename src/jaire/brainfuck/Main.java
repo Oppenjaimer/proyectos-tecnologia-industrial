8d54b706d8b16a08c02f7f2cbb565876f03662a3
JaimermXD <jairegra@gmail.com>
2022-05-29 16:52:17 +0200

    Implement command-line interpreting

--- src/jaire/brainfuck/Main.java
+++ src/jaire/brainfuck/Main.java
@@ -1,7 +1,23 @@
 package jaire.brainfuck;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Interpreter interpreter = new Interpreter();
+
     public static void main(String[] args) {
-        //...
+        if (args.length < 1) {
+            while (true) {
+                System.out.print("Brainfuck > ");
+                String code = scanner.nextLine();
+
+                if (code.equals("ptr")) System.out.printf("ptr: %d%n", interpreter.getPtr());
+                if (code.equals("mem")) System.out.printf("mem: %d%n", interpreter.getMem());
+                if (code.equals("exit")) break;
+
+                interpreter.interpret(code);
+            }
+        }
     }
 }
